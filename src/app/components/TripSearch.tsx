715d032be6d1b15e5c46b8b617442f0c9eaa3efd
claudivan <claudivansantos61@gmail.com>
2023-08-17 21:18:07 -0300

    feat: add search params

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,25 +1,83 @@
 "use client";
 
-import Button from "./Button";
-import CurrencyInput from "./CurrencyInput";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
 import DatePicker from "./DatePicker";
 import Input from "./Input";
+import CurrencyInput from "./CurrencyInput";
+import Button from "./Button";
 
-export default function TripSearch() {
+interface TripSearchForm {
+  text: string;
+  startDate: Date | null;
+  budget: string;
+}
+
+const TripSearch = () => {
+  const router = useRouter();
+
+  const {
+    control,
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<TripSearchForm>();
+
+  const onSubmit = (data: TripSearchForm) => {
+    router.push(`/trips/search?text=${data.text}&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`);
+  };
+
   return (
-    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
-      <h1 className="font-semibold text-2xl text-primaryDarker text-center">
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
+      <h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h1>
-      <div className="flex flex-col gap-4 mt-5">
-        <Input placeholder="Onde você quer ir" />
 
-        <div className="flex gap-4">
-          <DatePicker placeholder="Data de ida" className="w-full" onChange={()=> ''}/>
-          <CurrencyInput placeholder="Orçamento"/>
+      <div className="flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg">
+        <Input
+          placeholder="Onde você quer ir?"
+          error={!!errors.text}
+          errorMessage={errors.text?.message}
+          {...register("text", {
+            required: {
+              value: true,
+              message: "Texto é obrigatório.",
+            },
+          })}
+        />
+
+        <div className="flex gap-4 lg:w-full">
+          <Controller
+            name="startDate"
+            control={control}
+            render={({ field }) => (
+              <DatePicker onChange={field.onChange} selected={field.value} placeholder="Data Final" className="w-full" minDate={new Date()} />
+            )}
+          />
+
+          <Controller
+            name="budget"
+            control={control}
+            render={({ field }) => (
+              <CurrencyInput
+                allowDecimals={false}
+                placeholder="Orçamento"
+                onValueChange={field.onChange as any}
+                value={field.value}
+                onBlur={field.onBlur}
+                className="w-full"
+              />
+            )}
+          />
         </div>
-        <Button>Buscar</Button>
+
+        <Button onClick={() => handleSubmit(onSubmit)()} className="lg:w-1/2 lg:h-fit">
+          Buscar
+        </Button>
       </div>
     </div>
   );
-}
+};
+
+export default TripSearch;
